c9566eef913f1b9b80aa17b4e785d0fb13b8da44
Daniel Ketterer <26778610+dketterer@users.noreply.github.com>
2021-04-15 23:40:46 +0200

    fix in-place modification in PSNR

--- colorization/metrics/psnr.py
+++ colorization/metrics/psnr.py
@@ -1,20 +1,21 @@
 import torch
+from torch import nn
 
 
-class PSNR:
+class PSNR(nn.Module):
     """Peak Signal to Noise Ratio
     img1 and img2 have range [-1,1]"""
 
     def __init__(self):
+        super().__init__()
         self.name = "PSNR"
 
-    @staticmethod
-    def __call__(img1, img2):
-        img1 /= 2.
-        img1 += 0.5
-        img1 *= 255.
-        img2 /= 2.
-        img2 += 0.5
-        img2 *= 255.
-        mse = torch.mean((img1 - img2) ** 2)
+    def forward(self, img1, img2):
+        img1_copy = img1.divide(2)
+        img2_copy = img2.divide(2)
+        img1_copy += 0.5
+        img1_copy *= 255.
+        img2_copy += 0.5
+        img2_copy *= 255.
+        mse = torch.mean((img1_copy - img2_copy) ** 2)
         return 20 * torch.log10(255.0 / torch.sqrt(mse))

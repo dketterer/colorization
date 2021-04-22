91758262181b21725e122ef7b1c7036213df8a8f
Daniel Ketterer <26778610+dketterer@users.noreply.github.com>
2021-04-22 18:15:24 +0200

    Add rotation and flip to augmentation template

--- resources/transform.py
+++ resources/transform.py
@@ -4,7 +4,10 @@ from colorization.augmentation import LimitMaxSize
 
 def get_transform(size):
     transform = A.Compose(
-        [A.RandomResizedCrop(size, size, scale=(0.7, 1.0), ratio=(0.9, 1.1))
+        [A.RandomResizedCrop(size, size, scale=(0.7, 1.0), ratio=(0.9, 1.1), always_apply=True),
+         A.RandomRotate90(p=0.3),
+         A.HorizontalFlip(p=0.3),
+         A.VerticalFlip(p=0.1)
          ])
     return transform
 

aeebc1d065f9af94f355e61f995683917c17ee2e
Daniel Ketterer <26778610+dketterer@users.noreply.github.com>
2021-06-02 14:08:10 +0200

    Update Test for CCL

--- test/test_color_consitency_loss.py
+++ test/test_color_consitency_loss.py
@@ -5,7 +5,7 @@ import torch
 from torch import Tensor
 from torch.cuda.amp import autocast
 
-from colorization.loss import ColorConsistencyLoss, L1CCLoss
+from colorization.loss import ColorConsistencyLoss, L1CCLoss, ColorConsistencyLoss
 
 
 class TestColorConsistencyLoss(unittest.TestCase):
@@ -52,12 +52,9 @@ class TestColorConsistencyLoss(unittest.TestCase):
                             [[1, -1, -3],
                              [0, 0, 0],
                              [3, 0, 0]]]], dtype=torch.float, requires_grad=True)
-        segments = torch.unsqueeze(torch.stack([torch.tensor([[True, True, True],
-                                                              [True, True, False],
-                                                              [True, False, False]]),
-                                                torch.tensor([[False, False, False],
-                                                              [False, False, True],
-                                                              [False, True, True]])]), 0)
+        segments = torch.unsqueeze(torch.tensor([[0, 0, 0],
+                                                 [0, 0, 1],
+                                                 [0, 1, 1]]), 0)
         loss = ColorConsistencyLoss('square')
 
         l = loss(ab, segments)
@@ -67,6 +64,15 @@ class TestColorConsistencyLoss(unittest.TestCase):
             self.fail()
 
         l.backward()
+
+        self.assertTrue(torch.all(torch.isclose(torch.tensor([[[[0.1111, -0.1111, -0.1111],
+                                                                [-0.2222, -0.3333, 0.0000],
+                                                                [0.6667, 0.0000, 0.0000]],
+
+                                                               [[0.1111, -0.1111, -0.3333],
+                                                                [0.0000, 0.0000, 0.0000],
+                                                                [0.3333, 0.0000, 0.0000]]]]), ab.grad, rtol=1e-4,
+                                                atol=1e-6)))
 
     def test_linear(self):
         ab = torch.tensor([[[[5, 3, 3],
@@ -91,7 +97,7 @@ class TestColorConsistencyLoss(unittest.TestCase):
         except:
             self.fail()
 
-    def test_compund_on_cuda(self):
+    def test_compound_on_cuda(self):
         ab = torch.tensor([[[[5, 3, 3],
                              [2, 1, 0],
                              [10, 0, 0]],
@@ -103,7 +109,7 @@ class TestColorConsistencyLoss(unittest.TestCase):
                                                               [True, False, False]]),
                                                 torch.tensor([[False, False, False],
                                                               [False, False, True],
-                                                              [False, True, True]])]), 0).cuda()
+                                                              [False, True, True]])]), 0).long().cuda()
         loss = L1CCLoss(0.5, 'linear').cuda()
         l, dic = loss(ab, ab, segments)
         l.backward()
@@ -141,6 +147,52 @@ class TestColorConsistencyLoss(unittest.TestCase):
             np.testing.assert_almost_equal(0.944, l.detach().numpy(), 3)
         except:
             self.fail()
+
+    def test_squared_masks_V2(self):
+        ab = torch.tensor([[[[5, 3, 3],
+                             [2, 1, 0],
+                             [10, 0, 0]],
+                            [[1, -1, -3],
+                             [0, 0, 0],
+                             [3, 0, 0]]]], dtype=torch.float, requires_grad=True)
+        segments = torch.unsqueeze(torch.tensor([[0, 0, 0],
+                                                 [0, 0, 1],
+                                                 [0, 1, 1]]), 0)
+        loss = ColorConsistencyLoss('square')
+
+        l = loss(ab, segments)
+        try:
+            np.testing.assert_almost_equal(4.0, l.detach().numpy(), 3)
+        except:
+            self.fail()
+
+        l.backward()
+
+    def test_linear_masks_v2(self):
+        ab = torch.tensor([[[[5, 3, 3],
+                             [2, 1, 0],
+                             [10, 0, 0]],
+                            [[1, -1, -3],
+                             [0, 0, 0],
+                             [3, 0, 0]]]], dtype=torch.float, requires_grad=True)
+        segments = torch.unsqueeze(torch.tensor([[0, 0, 0],
+                                                 [0, 0, 1],
+                                                 [0, 1, 1]]), 0)
+        loss = ColorConsistencyLoss('linear')
+
+        l = loss(ab, segments)
+        try:
+            # result is 1.2222 for real l1_loss, but smooth_l1_loss behaves a little different
+            np.testing.assert_almost_equal(0.944, l.detach().numpy(), 3)
+        except:
+            self.fail()
+
+    def test_linear_masks_v2_batch(self):
+        ab = torch.ones(3, 2, 30, 30)
+        segments = torch.cat([torch.zeros(2, 30, 30), torch.arange(30 * 30).reshape(1, 30, 30)], 0).long()
+        loss = ColorConsistencyLoss('linear')
+
+        l = loss(ab, segments)
 
 
 if __name__ == '__main__':
